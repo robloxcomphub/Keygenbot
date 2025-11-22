a96f9f945775584dfe23446eedd0e9d848df8199
robloxcomphub <132360328+robloxcomphub@users.noreply.github.com>
2025-11-22 18:25:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Webserver running on port ${PORT}`);
+});
+
+
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const { handleCommand } = require('./commands');
